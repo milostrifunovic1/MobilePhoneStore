d277d6664e6566cdd75b74ede7dece1bccd75f58
milostrifunovic1 <42545699+milostrifunovic1@users.noreply.github.com>
2018-11-27 22:38:54 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,166 @@
+
+$(document).ready(function() {
+    $(".se-pre-con").fadeOut(1500);
+});
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+} 
+
+
+function back() {
+    document.getElementById("bck1").innerHTML = document.getElementById("bck3").innerHTML;
+    document.getElementById("back").style.display = "none";
+};
+
+function p20pro(){
+    document.getElementById("bck1").innerHTML = document.getElementById("p20pro").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function s9plus(){
+    document.getElementById("bck1").innerHTML = document.getElementById("s9plus").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function applex(){
+    document.getElementById("bck1").innerHTML = document.getElementById("appx").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function xa2(){
+    document.getElementById("bck1").innerHTML = document.getElementById("xa2").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function honor9(){
+    document.getElementById("bck1").innerHTML = document.getElementById("honor9").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function galaxya5(){
+    document.getElementById("bck1").innerHTML = document.getElementById("galaxya5").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function n7plus(){
+    document.getElementById("bck1").innerHTML = document.getElementById("n7plus").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function motox4(){
+    document.getElementById("bck1").innerHTML = document.getElementById("motox4").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function apple7(){
+    document.getElementById("bck1").innerHTML = document.getElementById("apple7").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function apple8plus(){
+    document.getElementById("bck1").innerHTML = document.getElementById("apple8plus").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function applese(){
+    document.getElementById("bck1").innerHTML = document.getElementById("applese").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function pixel2xl(){
+    document.getElementById("bck1").innerHTML = document.getElementById("pixel2xl").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function htcu12plus(){
+    document.getElementById("bck1").innerHTML = document.getElementById("htcu12plus").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function htcuultra(){
+    document.getElementById("bck1").innerHTML = document.getElementById("htcuultra").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function honor10(){
+    document.getElementById("bck1").innerHTML = document.getElementById("honor10").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function p20lite(){
+    document.getElementById("bck1").innerHTML = document.getElementById("p20lite").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function lgg7(){
+    document.getElementById("bck1").innerHTML = document.getElementById("lgg7").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function lgq8(){
+    document.getElementById("bck1").innerHTML = document.getElementById("lgq8").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function motoz2play(){
+    document.getElementById("bck1").innerHTML = document.getElementById("motoz2play").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function nokia8(){
+    document.getElementById("bck1").innerHTML = document.getElementById("nokia8").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function oneplus6(){
+    document.getElementById("bck1").innerHTML = document.getElementById("oneplus6").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function galaxya8(){
+    document.getElementById("bck1").innerHTML = document.getElementById("galaxya8").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function note8(){
+    document.getElementById("bck1").innerHTML = document.getElementById("note8").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function galaxys7(){
+    document.getElementById("bck1").innerHTML = document.getElementById("galaxys7").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function galaxys8plus(){
+    document.getElementById("bck1").innerHTML = document.getElementById("galaxys8plus").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function xperiax(){
+    document.getElementById("bck1").innerHTML = document.getElementById("xperiax").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function xz2premium(){
+    document.getElementById("bck1").innerHTML = document.getElementById("xz2premium").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
+function xiaomimi6(){
+    document.getElementById("bck1").innerHTML = document.getElementById("xiaomimi6").innerHTML;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("back").style.display = "flex";
+};
